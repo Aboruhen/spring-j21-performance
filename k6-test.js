a3f35a6e579cf489fce66a43a8229e40171c7634
Roman Kniaz <aboruhen@gmail.com>
2023-10-02 12:45:51 +0300

    add high load with 10m int sum; + 2_000 users

--- k6-test.js
+++ k6-test.js
@@ -4,8 +4,8 @@ export const options = {
   stages: [
     {duration: '10s', target: 20},
     {duration: '10s', target: 300},
-    // {duration: '10s', target: 1000},
-    // {duration: '10s', target: 2000},
+    {duration: '10s', target: 1000},
+    {duration: '10s', target: 2000},
     {duration: '10s', target: 100},
   ],
 };
--- main-performance/src/main/java/com/virtualthread/springperformance/service/HighLoadService.java
+++ main-performance/src/main/java/com/virtualthread/springperformance/service/HighLoadService.java
@@ -2,16 +2,27 @@ package com.virtualthread.springperformance.service;
 
 import java.math.BigDecimal;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerErrorException;
 
 @Service
 public class HighLoadService {
 
     public String calculateScore() {
         Random random = new Random();
-        return Stream.generate(random::nextInt).limit(10_000_000)
-            .map(BigDecimal::valueOf).reduce(BigDecimal.ZERO, BigDecimal::add).toPlainString();
+        try (ExecutorService myExecutor =
+            Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<String> submit = myExecutor.submit(() -> Stream.generate(random::nextInt).limit(10_000_000)
+                .map(BigDecimal::valueOf).reduce(BigDecimal.ZERO, BigDecimal::add).toPlainString());
+            return submit.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new ServerErrorException("Error", e);
+        }
     }
 
 }
