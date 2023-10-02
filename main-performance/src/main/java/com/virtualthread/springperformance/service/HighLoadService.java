3bbd634ac30711f85b93f771b58f1e668a0c81c8
Roman Kniaz <aboruhen@gmail.com>
2023-10-02 12:18:39 +0300

    add high load with 10m int sum

--- main-performance/src/main/java/com/virtualthread/springperformance/service/HighLoadService.java
+++ main-performance/src/main/java/com/virtualthread/springperformance/service/HighLoadService.java
@@ -1,12 +1,17 @@
 package com.virtualthread.springperformance.service;
 
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HighLoadService {
 
     public String calculateScore() {
-        return "100000";
+        Random random = new Random();
+        return Stream.generate(random::nextInt).limit(10_000_000)
+            .map(BigDecimal::valueOf).reduce(BigDecimal.ZERO, BigDecimal::add).toPlainString();
     }
 
 }
